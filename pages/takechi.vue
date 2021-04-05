c28f0ecf1b0d939f9472c4aad8b705097ff73cfd
近藤岳瑠 <takechihorn@gmail.com>
2021-04-06 01:43:50 +0900

    first commit

---
+++ pages/takechi.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h3>{{ data.message }}</h3>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from '@vue/composition-api'
+
+export default defineComponent({
+  setup() {
+    const data = ref({
+      message: 'Hello World',
+      firstName: 'takeru',
+      lastName: 'kondo',
+    })
+    return { data }
+  },
+})
+</script>
+
+<style scoped></style>
